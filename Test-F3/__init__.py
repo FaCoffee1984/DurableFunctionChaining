4548524b5eb02705a0bf3dfdd363ba0fa46a6a7c
FaCoffee1984 <60174218+FaCoffee1984@users.noreply.github.com>
2021-01-05 15:31:59 +0000

    Add files via upload

---
+++ Test-F3/__init__.py
@@ -0,0 +1,31 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an orchestrator function.
+# Before running this sample, please:
+# - create a Durable orchestration function
+# - create a Durable HTTP starter function
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+
+import logging
+from azure.cosmos import CosmosClient
+import azure.functions as func
+import time
+
+
+def add_date(string=None):
+    '''Generate today's timestamp'''
+
+    today = time.strftime("%Y-%m-%d, %H:%M:%S")
+
+    return today
+
+
+def main(F2activitytrigger, orchestrationResult2, outputDocument: func.Out[func.Document]) -> str:
+
+    result3 = orchestrationResult2.copy()
+
+    result3['timestamp'] = add_date()
+
+    outputDocument.set(func.Document.from_dict(result3))
+
+    return
